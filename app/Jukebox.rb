01d080a69aad66003477c7431d77138546e8db9b
Bleak Chandler <bleakchandler@gmail.com>
2021-02-17 22:25:42 -0500

    added some changes

--- app/Jukebox.rb
+++ app/Jukebox.rb
@@ -3,37 +3,42 @@ class Jukebox
   attr_accessor :user, :password
   
   def initialize
-
     #config this out later
     @prompt = TTY::Prompt.new
   end
   
   def run
       welcome
-    # login_or_signup
-    # wanna_see_favs?
-    # some_method(some_argument)
-    # exit
   end
   
   def welcome
+    system 'clear'
     #welcome_music
     sleep(1)
     puts "Welcome to...".colorize(:magenta)
     sleep(1)
-    #banner #set this up
-    sleep(2)
+    banner #set this up
+    sleep(1)
     prompt.select("Sign in or Sign up") do |option|
       option.choice "Sign in", -> {sign_in_helper}
       option.choice "Sign up", -> {sign_up_helper}
       option.choice "Exit", -> {exit_helper}
     end
   end
-  
+
+  def banner
+    puts "
+    ████████ ██   ██ ███████          ██ ██    ██ ██   ██ ███████ ██████   ██████  ██   ██ 
+       ██    ██   ██ ██               ██ ██    ██ ██  ██  ██      ██   ██ ██    ██  ██ ██  
+       ██    ███████ █████            ██ ██    ██ █████   █████   ██████  ██    ██   ███   
+       ██    ██   ██ ██          ██   ██ ██    ██ ██  ██  ██      ██   ██ ██    ██  ██ ██  
+       ██    ██   ██ ███████      █████   ██████  ██   ██ ███████ ██████   ██████  ██   ██                                                                                                  
+    ".colorize(:magenta)
+  end
   
   def sign_in_helper
     system 'clear'
-    name = prompt.ask("What is your username?")
+    name = prompt.ask("Enter your username:")
     if self.user = User.find_by(name: name)
       puts "Welcome back #{user.name}!"
       main_menu_welcome_back
@@ -47,10 +52,11 @@ class Jukebox
     system 'clear'
     name = prompt.ask("What would you like your username to be?")
     while User.find_by(name: name)
-      puts "This username already exists."
-      name = prompt.ask("What would you like your username to be?")
+      puts "This username already exists. Please enter a new username."
+      sleep(2)
+      sign_up_helper
     end
-    password = prompt.ask("Enter a password")
+    password = prompt.ask("Enter a password:")
     self.user = User.create(name: name, password: password)
     main_menu_new_user
   end 
@@ -59,7 +65,7 @@ class Jukebox
     system 'clear'
     puts "Welcome back, #{user.name}!"
     #banner?
-    prompt.select("Browse or View list") do |option|
+    prompt.select("Browse or view list") do |option|
       option.choice "Browse categories", -> {categories_helper}
       option.choice "View favorites", -> {favorites_helper}
       option.choice "Exit", -> {exit_helper}
@@ -70,7 +76,7 @@ class Jukebox
     system 'clear'
     puts "Welcome, #{user.name}!"
     #banner?
-    prompt.select("Browse or View list") do |option|
+    prompt.select("Browse or view list") do |option|
       option.choice "Browse categories", -> {categories_helper}
       option.choice "Exit", -> {exit_helper}
     end
@@ -91,16 +97,13 @@ class Jukebox
     system 'clear'
     genre = Genre.all_genres
     genre_name = prompt.select("Which Genre would you like?", genre)
-    Genre.all_songs_by_genre(genre_name)
+    binding.pry
+    user.all_songs_by_genre(genre_name)
       # prompt.select(song_collection) do |options|
       #   option.choice "Enter Song ID", -> ()
       #   option.choice "Choose a different genre", -> (genres_helper)
       #   option.choice "Exit" -> (exit_helper)
   end 
-
-  def see_all_songs_for_genre
-
-  end
 
   def artists_helper
     system 'clear'
@@ -125,18 +128,10 @@ class Jukebox
     Favorite.all.select{|favorite| favorite.user_id == user.id}
   end
 
-  # def albums_helper
-  #   album = Album.all_albums
-  #     album_id = prompt.select("Which Album would you like?", album)
-  #   album_selection = user.create_selection_by_album(album_id)
-  # end 
-
   def exit_helper
     puts "See you next time!"
     sleep(2)
     system 'clear'
     exit
   end
-
-  # private
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,13 @@
-# THIS SEED FILE NEEDS TO BE ENTIRELY REPLACED -- I'M LEAVING CODE FOR YOUR REFERENCE ONLY!
-
-# User.destroy_all
-# Artist.destroy_all
-# Genre.destroy_all
-# Album.destroy_all
-# Song.destroy_all
-# Favorite.destroy_all
-# User.reset_pk_sequence
-# Artist.reset_pk_sequence
-# Genre.reset_pk_sequence
-# Album.reset_pk_sequence
-# Song.reset_pk_sequence
-# Favorite.reset_pk_sequence
-
-
-########### different ways to write your seeds ############
-
-# 1: save everything to variables (makes it easy to connect models, best for when you want to be intentional about your seeds)
+User.destroy_all
+Artist.destroy_all
+Genre.destroy_all
+Song.destroy_all
+Favorite.destroy_all
+User.reset_pk_sequence
+Artist.reset_pk_sequence
+Genre.reset_pk_sequence
+Song.reset_pk_sequence
+Favorite.reset_pk_sequence
 
 #users
 bleak = User.create(name: "codingcat", password: "cat")
@@ -26,82 +17,64 @@ jamie = User.create(name: "ninjacat", password: "911")
 taylor = Artist.create(name: "Taylor Swift")
 red_hot = Artist.create(name: "Red Hot Chili Peppers")
 daft = Artist.create(name: "Daft Punk")
+yaeji = Artist.create(name: "Yaeji")
+loffler = Artist.create(name: "Christian Löffler")
+gold_fir = Artist.create(name: "Gold Fir")
+eli_and_fur = Artist.create(name: "Eli & Fur")
+shygirl = Artist.create(name: "Shygirl")
+robin_s = Artist.create(name: "Robin S.")
+rina = Artist.create(name: "Rina Sawayama")
+rac = Artist.create(name: "RAC")
+strongboi = Artist.create(name: "Strongboi")
+cautious = Artist.create(name: "Cautious Clay")
+mor = Artist.create(name: "MorMor")
+fx = Artist.create(name: "f(x)")
+charli = Artist.create(name: "Charli XCX")
+gaga = Artist.create(name: "Lady Gaga")
+fallout = Artist.create(name: "Fall Out Boy")
+frank = Artist.create(name: "Frank Ocean")
+vince = Artist.create(name: "Vince Staples")
+breaking = Artist.create(name: "Breaking Benjamin")
 
 #genres
 country = Genre.create(name: "Country")
 alt = Genre.create(name: "Alternative")
 elec = Genre.create(name: "Electronic")
+country = Genre.create(name: "Country")
+alt = Genre.create(name: "Alternative")
+elec = Genre.create(name: "Electronic")
+hip_hop = Genre.create(name: "Hip-Hop")
+house = Genre.create(name: "House")
+indie = Genre.create(name: "Indie")
+k_pop = Genre.create(name: "K-Pop")
+pc = Genre.create(name: "PC Music")
+pop = Genre.create(name: "Pop")
+punk = Genre.create(name: "Punk")
+r_b = Genre.create(name: "R&B")
+rap = Genre.create(name: "Rap")
+rock = Genre.create(name: "Rock")
 
 #songs
 love_story = Song.create(name: "Love Story", artist_id: taylor.id, length: "3:56", genre_id: country.id, year: 2008)
 californication = Song.create(name: "Californication", artist_id: red_hot.id, length: "5:30", genre_id: alt.id, year: 1999)
 harder = Song.create(name: "Harder, Better, Faster, Stronger", artist_id: daft.id, length: "3:45", genre_id: elec.id, year: 2001)
+rain_gurl = Song.create(name: "Raingurl", artist_id: yaeji.id, length: "3:57", genre_id: elec.id, year: 2017)
+ronda = Song.create(name: "Ronda", artist_id: loffler.id, length: "5:46", genre_id: elec.id, year: 2020)
+night_walk = Song.create(name: "Night Walk", artist_id: gold_fir.id, length: "4:18", genre_id: elec.id, year: 2019)
+something_was_real = Song.create(name: "Something Was Real", artist_id: eli_and_fur.id, length: "3:42", genre_id: elec.id, year: 2018)
+slime = Song.create(name: "SLIME", artist_id: shygirl.id, length: "2:39", genre_id: hip_hop.id, year: 2020)
+show_me_love = Song.create(name: "Show Me Love", artist_id: robin_s.id, length: "4:12", genre_id: house.id, year: 1993)
+xs = Song.create(name: "XS", artist_id: rina.id, length: "3:21", genre_id: indie.id, year: 2020)
+mia = Song.create(name: "MIA", artist_id: rac.id, length: "2:32", genre_id: indie.id, year: 2020)
+honey = Song.create(name: "Honey Thighs", artist_id: strongboi.id, length: "4:44", genre_id: indie.id, year: 2020)
+cold_war = Song.create(name: "Cold War", artist_id: cautious.id, length: "3:22", genre_id: indie.id, year: 2018)
+heaven = Song.create(name: "Heaven's Only Wishful", artist_id: mor.id, length: "4:03", genre_id: indie.id, year: 2018)
+walls = Song.create(name: "4 Walls", artist_id: fx.id, length: "3:27", genre_id: k_pop.id, year: 2015)
+ten = Song.create(name: "Track 10", artist_id: charli.id, length: "5:27", genre_id: pc.id, year: 2017)
+stupid = Song.create(name: "Stupid Love", artist_id: gaga.id, length: "3:14", genre_id: pop.id, year: 2020)
+sugar = Song.create(name: "Sugar, We’re Goin Down", artist_id: fallout.id, length: "3:49", genre_id: fallout.id, year: 2005)
+pyramids = Song.create(name: "Pyramids", artist_id: frank.id, length: "9:53", genre_id: r_b.id, year: 2012)
+yeah_right = Song.create(name: "Yeah Right", artist_id: vince.id, length: "3:09", genre_id: rap.id, year: 2017)
+diary = Song.create(name: "Diary of Jane", artist_id: breaking.id, length: "3:21", genre_id: rock.id, year: 2006)
 
-
-
-
-
-
-
-
-
-# basil = Plant.create(name: "basil the herb", bought: 20200610, color: "green")
-# sylwia = Person.create(name: "Sylwia", free_time: "none", age: 30)
-# pp1 = PlantParenthood.create(plant_id: basil.id, person_id: sylwia.id, affection: 1_000_000, favorite?: true)
-
-# # 2. Mass create -- in order to connect them later IN SEEDS (not through the app) you'll need to find their id
-# ## a. by passing an array of hashes:
-# Plant.create([
-#     {name: "Corn Tree", bought: 20170203, color: "green"},
-#     {name: "Prayer plant", bought: 20190815, color: "purple"},
-#     {name: "Cactus", bought: 20200110, color: "ugly green"}
-# ])
-# ## b. by interating over an array of hashes:
-# plants = [{name: "Elephant bush", bought: 20180908, color: "green"},
-#     {name: "Photos", bought: 20170910, color: "green"},
-#     {name: "Dragon tree", bought: 20170910, color: "green"},
-#     {name: "Snake plant", bought: 20170910, color: "dark green"},
-#     {name: "polka dot plant", bought: 20170915, color: "pink and green"},
-#     {name: "Cactus", bought: 20200517, color: "green"}]
-
-# plants.each{|hash| Plant.create(hash)}
-
-# # 3. Use Faker and mass create
-# ## step 1: write a method that creates a person
-# def create_person
-#     free = ["mornings", "evenings", "always", "afternoons", "weekends", "none"].sample
-
-#     person = Person.create(
-#         name: Faker::Movies::HitchhikersGuideToTheGalaxy.character,
-#         free_time: free,
-#         age: rand(11...70)
-#     )
-# end
-
-# ## step 2: write a method that creates a joiner
-# def create_joiners(person)
-#     plants_number = rand(1..4)
-#     plants_number.times do 
-#         PlantParenthood.create(
-#             plant_id: Plant.all.sample.id, 
-#             person_id: person.id, 
-#             affection: rand(101), 
-#             favorite?: [true, false].sample
-#         )
-#     end
-# end
-
-# ## step 3: invoke creating joiners by passing in an instance of a person
-# 10.times do     
-#     create_joiners(create_person)
-#     ##### ALTERNATIVE:
-#     # person = create_person
-#     # create_joiners(person)
-# end
-
-# indoor = Category.create(name: "indoors")
-
-# Plant.update(category_id: indoor.id)
-
-
- puts "🔥 🔥 seeded 🔥 🔥 "
+ puts "💿 💿 💿 💿 Seeded! 💿 💿 💿 💿 "
