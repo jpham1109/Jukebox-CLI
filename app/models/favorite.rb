e3b11fc0112d1708083d470f830706f21fa87a5b
Bleak Chandler <bleakchandler@gmail.com>
2021-02-18 19:00:30 -0500

    added some changes

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,11 +2,11 @@ class Favorite < ActiveRecord::Base
     belongs_to :user
     belongs_to :song
 
-    def to_s
-        self.name 
-    end
+    # def to_s
+    #     self.name 
+    # end
 
-    def self.all_favorites
-        self.all.map{|song| song.song_id}  
-    end
+    # def self.all_favorites
+    #     self.all.map{|song| song.song_id}  
+    # end
 end
